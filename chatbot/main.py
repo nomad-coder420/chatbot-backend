e8c08b4751a82b7c7693e47fec328a4aa8090671
nomad-coder420 <saketmahajan0@gmail.com>
2025-01-25 21:01:54 +0530

    middlewares added

--- chatbot/main.py
+++ chatbot/main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from chatbot.api.v1.router import router as v1_router
+from chatbot.middlewares.exceptions import ExceptionMiddleware
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+app.include_router(v1_router, prefix="/api/v1")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This now allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.add_middleware(ExceptionMiddleware)
---
+++ chatbot/middlewares/exceptions.py
@@ -0,0 +1,22 @@
+from starlette.middleware.base import BaseHTTPMiddleware, RequestResponseEndpoint
+from starlette.responses import Response
+from fastapi.responses import JSONResponse
+from starlette.requests import Request
+from fastapi import status
+
+
+class ExceptionMiddleware(BaseHTTPMiddleware):
+    async def dispatch(
+        self, request: Request, call_next: RequestResponseEndpoint
+    ) -> Response:
+        try:
+            response = await call_next(request)
+        except Exception as e:
+            print(e)
+
+            response = JSONResponse(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                content={"detail": {"error": str(e)}},
+            )
+
+        return response
